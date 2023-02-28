3ab08a39d22fa530118e73b4244554e57b2528e3
emon3455 <emon.mhk69@gmail.com>
2023-03-01 02:05:31 +0600

    partially done project

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const  displayData=(players)=>{
     const conatiner = document.getElementById("search-Result-container");
     conatiner.innerHTML="";
     const random = "https://picsum.photos/600/300"
-    players.slice(0,2).forEach(player => {
+    players.forEach(player => {
         const {strThumb,strPlayer,strDescriptionIT,strNationality,idPlayer} = player;
         const div = document.createElement("div");
         div.innerHTML=`
@@ -35,10 +35,59 @@ const  displayData=(players)=>{
 }
 
 
+const singleLoad=(id)=>{
+    const url = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${id}`;
+    fetch(url)
+    .then (res=>res.json())
+    .then (data=>displaySingleData(data.players[0]))
+    .catch(ex=>{
+        console.log(ex);
+    });
+}
+
+const displaySingleData=(data)=>{
+   
+    const {strGender} = data;
+    if(strGender==="Male"){
+        document.getElementById("banner1").classList.add("hidden");
+        document.getElementById("banner2").classList.remove("hidden"); 
+    }
+    else{
+        document.getElementById("banner1").classList.remove("hidden");
+        document.getElementById("banner2").classList.add("hidden");
+    }
+
+    const {strThumb,strPlayer,strDescriptionIT,strNationality,strHeight,strFacebook,strInstagram} = data;
+    const container = document.getElementById("player-info");
+    container.innerHTML=`
+        <div class="card card-side bg-base-100 shadow-xl">
+            <div>
+                <img src="${strThumb}" alt="Movie"/>
+            </div>
+            <div class="card-body">
+                <h2 class="card-title">${strPlayer}</h2>
+                <p>${strNationality}</p>
+                <p>${strDescriptionIT? strInstagram.slice(0,100)+"...":"No Details Available"}</p>
+                <p>${strHeight}</p>
+                <p>
+                   <span class="font-semibold">Facebook: </span> ${strFacebook} 
+                </p>
+                <p>
+                <span class="font-semibold">Instagram: </span> ${strInstagram} 
+                </p>
+            </div>
+        </div>
+    `;
+    
+    
+}
+
 document.getElementById("search-btn").addEventListener("click",()=>{
+    
     const inputValue = document.getElementById("input-field").value;
     loadData(inputValue);
+    
 });
 
-loadData("messi");
+// loadData("messi");
 
